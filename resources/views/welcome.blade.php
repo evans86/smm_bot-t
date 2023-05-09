6a858c927eda1d2d1b3c112120fa49830134b856
evans86 <evans86@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>
2023-05-09 21:49:42 +0300

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,8 @@
 @section('content')
 <div class="container h-400 d-flex justify-content-center">
     <div class="jumbotron my-auto">
-        <h1 class="display-3" style="box-shadow: 1px 5px 23px 3px rgba(34, 60, 80, 0.26);">
-            <img src="{{ asset('img/collage.png' )}}">
+        <h1 class="display-3">
+            <img src="{{ asset('img/collage1.svg' )}}">
         </h1>
     </div>
 </div>
