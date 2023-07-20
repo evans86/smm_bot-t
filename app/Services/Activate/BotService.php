e4ac5bf9348f6035d2177d2b19483df542af1f11
evans86 <evans86@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>
2023-07-21 01:38:26 +0300

    add all log

--- app/Services/Activate/BotService.php
+++ app/Services/Activate/BotService.php
@@ -46,7 +46,7 @@ class BotService extends MainService
      */
     public function update(BotDto $dto): Bot
     {
-        $bot = Bot::query()->where('publicfdhdf_key', $dto->public_key)->where('private_key', $dto->private_key)->first();
+        $bot = Bot::query()->where('public_key', $dto->public_key)->where('private_key', $dto->private_key)->first();
         if (empty($bot))
             return ApiHelpers::error('Not found module.');
         $bot->version = $dto->version;
