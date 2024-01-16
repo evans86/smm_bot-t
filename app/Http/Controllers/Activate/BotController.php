dee3d26d52b6073f9da4c816b0d2704a0cbfd0ee
evans86 <evans86@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>
2024-01-17 00:01:02 +0300

    update api

--- app/Http/Controllers/Activate/BotController.php
+++ app/Http/Controllers/Activate/BotController.php
@@ -13,7 +13,7 @@ class BotController
     {
         $bots = Bot::orderBy('id', 'DESC')->Paginate(10);
 
-        $newBots = count(Bot::query()->where('created_at', '>', '2023-12-10 16:37:20')->get());
+        $newBots = count(Bot::query()->where('created_at', '>', '2024-01-10 16:37:20')->get());
         $allCount = count(Bot::get());
 
         return view('activate.bot.index', compact(
