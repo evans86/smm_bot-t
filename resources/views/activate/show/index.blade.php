cf6c5748aea87527a45ed359260b3162b1504675
evans <evandaniil85@gmail.com>
2024-08-22 21:12:50 +0300

    update api

--- resources/views/activate/show/index.blade.php
+++ resources/views/activate/show/index.blade.php
@@ -1,9 +1,5 @@
 @section('content')
-<html><head>
-    <title>Subscription Information</title>
-</head>
 
-<body>
     <h1>User Information</h1>
     <p>Username: 2116734017</p>
     <p>Status: <span class="status active">active</span></p>
@@ -12,52 +8,4 @@
         every day)</p>
     <p>Expiration Date:
 
-
-
-        2024-09-01 19:40:23 (10 days
-        remaining)
-        </p>
-
-
-    <h2>Links:</h2>
-    <ul>
-
-        <li>
-            <input type="text" value="vless://eece493c-eb13-42e0-94b9-928500855592@nodes.mrzb.artydev.ru:8443?security=reality&amp;type=tcp&amp;host=&amp;headerType=&amp;flow=xtls-rprx-vision&amp;path=&amp;sni=discordapp.com&amp;fp=chrome&amp;pbk=SbVKOEMjK0sIlbwg4akyBg5mL5KZwwB-ed4eEE7YnRc&amp;sid=&amp;spx=#%F0%9F%87%B7%F0%9F%87%B4%20A2116734017%20%5BVLESS%5D" readonly="">
-            <button class="copy-button" onclick="copyLink('vless://eece493c-eb13-42e0-94b9-928500855592@nodes.mrzb.artydev.ru:8443?security=reality&amp;type=tcp&amp;host=&amp;headerType=&amp;flow=xtls-rprx-vision&amp;path=&amp;sni=discordapp.com&amp;fp=chrome&amp;pbk=SbVKOEMjK0sIlbwg4akyBg5mL5KZwwB-ed4eEE7YnRc&amp;sid=&amp;spx=#%F0%9F%87%B7%F0%9F%87%B4%20A2116734017%20%5BVLESS%5D', this)">Copy</button>
-            <button class="qr-button" data-link="vless://eece493c-eb13-42e0-94b9-928500855592@nodes.mrzb.artydev.ru:8443?security=reality&amp;type=tcp&amp;host=&amp;headerType=&amp;flow=xtls-rprx-vision&amp;path=&amp;sni=discordapp.com&amp;fp=chrome&amp;pbk=SbVKOEMjK0sIlbwg4akyBg5mL5KZwwB-ed4eEE7YnRc&amp;sid=&amp;spx=#%F0%9F%87%B7%F0%9F%87%B4%20A2116734017%20%5BVLESS%5D">QR Code</button>
-        </li>
-
-        <li>
-            <input type="text" value="vless://eece493c-eb13-42e0-94b9-928500855592@finland.nodes.mrzb.artydev.ru:8443?security=reality&amp;type=tcp&amp;host=&amp;headerType=&amp;flow=xtls-rprx-vision&amp;path=&amp;sni=discordapp.com&amp;fp=chrome&amp;pbk=SbVKOEMjK0sIlbwg4akyBg5mL5KZwwB-ed4eEE7YnRc&amp;sid=&amp;spx=#%F0%9F%87%AB%F0%9F%87%AE%20A2116734017%20%5BVLESS%5D" readonly="">
-            <button class="copy-button" onclick="copyLink('vless://eece493c-eb13-42e0-94b9-928500855592@finland.nodes.mrzb.artydev.ru:8443?security=reality&amp;type=tcp&amp;host=&amp;headerType=&amp;flow=xtls-rprx-vision&amp;path=&amp;sni=discordapp.com&amp;fp=chrome&amp;pbk=SbVKOEMjK0sIlbwg4akyBg5mL5KZwwB-ed4eEE7YnRc&amp;sid=&amp;spx=#%F0%9F%87%AB%F0%9F%87%AE%20A2116734017%20%5BVLESS%5D', this)">Copy</button>
-            <button class="qr-button" data-link="vless://eece493c-eb13-42e0-94b9-928500855592@finland.nodes.mrzb.artydev.ru:8443?security=reality&amp;type=tcp&amp;host=&amp;headerType=&amp;flow=xtls-rprx-vision&amp;path=&amp;sni=discordapp.com&amp;fp=chrome&amp;pbk=SbVKOEMjK0sIlbwg4akyBg5mL5KZwwB-ed4eEE7YnRc&amp;sid=&amp;spx=#%F0%9F%87%AB%F0%9F%87%AE%20A2116734017%20%5BVLESS%5D">QR Code</button>
-        </li>
-
-        <li>
-            <input type="text" value="vless://eece493c-eb13-42e0-94b9-928500855592@rus.nodes.mrzb.artydev.ru:8443?security=reality&amp;type=tcp&amp;host=&amp;headerType=&amp;flow=xtls-rprx-vision&amp;path=&amp;sni=discordapp.com&amp;fp=chrome&amp;pbk=SbVKOEMjK0sIlbwg4akyBg5mL5KZwwB-ed4eEE7YnRc&amp;sid=&amp;spx=#%F0%9F%87%B7%F0%9F%87%BA%20A2116734017%20%5BVLESS%5D" readonly="">
-            <button class="copy-button" onclick="copyLink('vless://eece493c-eb13-42e0-94b9-928500855592@rus.nodes.mrzb.artydev.ru:8443?security=reality&amp;type=tcp&amp;host=&amp;headerType=&amp;flow=xtls-rprx-vision&amp;path=&amp;sni=discordapp.com&amp;fp=chrome&amp;pbk=SbVKOEMjK0sIlbwg4akyBg5mL5KZwwB-ed4eEE7YnRc&amp;sid=&amp;spx=#%F0%9F%87%B7%F0%9F%87%BA%20A2116734017%20%5BVLESS%5D', this)">Copy</button>
-            <button class="qr-button" data-link="vless://eece493c-eb13-42e0-94b9-928500855592@rus.nodes.mrzb.artydev.ru:8443?security=reality&amp;type=tcp&amp;host=&amp;headerType=&amp;flow=xtls-rprx-vision&amp;path=&amp;sni=discordapp.com&amp;fp=chrome&amp;pbk=SbVKOEMjK0sIlbwg4akyBg5mL5KZwwB-ed4eEE7YnRc&amp;sid=&amp;spx=#%F0%9F%87%B7%F0%9F%87%BA%20A2116734017%20%5BVLESS%5D">QR Code</button>
-        </li>
-
-        <li>
-            <input type="text" value="vless://eece493c-eb13-42e0-94b9-928500855592@turkey.nodes.mrzb.artydev.ru:8443?security=reality&amp;type=tcp&amp;host=&amp;headerType=&amp;flow=xtls-rprx-vision&amp;path=&amp;sni=discordapp.com&amp;fp=chrome&amp;pbk=SbVKOEMjK0sIlbwg4akyBg5mL5KZwwB-ed4eEE7YnRc&amp;sid=&amp;spx=#%F0%9F%87%B9%F0%9F%87%B7%20A2116734017%20%5BVLESS%5D" readonly="">
-            <button class="copy-button" onclick="copyLink('vless://eece493c-eb13-42e0-94b9-928500855592@turkey.nodes.mrzb.artydev.ru:8443?security=reality&amp;type=tcp&amp;host=&amp;headerType=&amp;flow=xtls-rprx-vision&amp;path=&amp;sni=discordapp.com&amp;fp=chrome&amp;pbk=SbVKOEMjK0sIlbwg4akyBg5mL5KZwwB-ed4eEE7YnRc&amp;sid=&amp;spx=#%F0%9F%87%B9%F0%9F%87%B7%20A2116734017%20%5BVLESS%5D', this)">Copy</button>
-            <button class="qr-button" data-link="vless://eece493c-eb13-42e0-94b9-928500855592@turkey.nodes.mrzb.artydev.ru:8443?security=reality&amp;type=tcp&amp;host=&amp;headerType=&amp;flow=xtls-rprx-vision&amp;path=&amp;sni=discordapp.com&amp;fp=chrome&amp;pbk=SbVKOEMjK0sIlbwg4akyBg5mL5KZwwB-ed4eEE7YnRc&amp;sid=&amp;spx=#%F0%9F%87%B9%F0%9F%87%B7%20A2116734017%20%5BVLESS%5D">QR Code</button>
-        </li>
-
-        <li>
-            <input type="text" value="vless://eece493c-eb13-42e0-94b9-928500855592@netherlands.nodes.mrzb.artydev.ru:8443?security=reality&amp;type=tcp&amp;host=&amp;headerType=&amp;flow=xtls-rprx-vision&amp;path=&amp;sni=cdn.discordapp.com&amp;fp=chrome&amp;pbk=SbVKOEMjK0sIlbwg4akyBg5mL5KZwwB-ed4eEE7YnRc&amp;sid=&amp;spx=#%F0%9F%87%B3%F0%9F%87%B1%20A2116734017%20%5BVLESS%5D" readonly="">
-            <button class="copy-button" onclick="copyLink('vless://eece493c-eb13-42e0-94b9-928500855592@netherlands.nodes.mrzb.artydev.ru:8443?security=reality&amp;type=tcp&amp;host=&amp;headerType=&amp;flow=xtls-rprx-vision&amp;path=&amp;sni=cdn.discordapp.com&amp;fp=chrome&amp;pbk=SbVKOEMjK0sIlbwg4akyBg5mL5KZwwB-ed4eEE7YnRc&amp;sid=&amp;spx=#%F0%9F%87%B3%F0%9F%87%B1%20A2116734017%20%5BVLESS%5D', this)">Copy</button>
-            <button class="qr-button" data-link="vless://eece493c-eb13-42e0-94b9-928500855592@netherlands.nodes.mrzb.artydev.ru:8443?security=reality&amp;type=tcp&amp;host=&amp;headerType=&amp;flow=xtls-rprx-vision&amp;path=&amp;sni=cdn.discordapp.com&amp;fp=chrome&amp;pbk=SbVKOEMjK0sIlbwg4akyBg5mL5KZwwB-ed4eEE7YnRc&amp;sid=&amp;spx=#%F0%9F%87%B3%F0%9F%87%B1%20A2116734017%20%5BVLESS%5D">QR Code</button>
-        </li>
-
-    </ul>
-    <div class="qr-popup" id="qrPopup">
-        <div class="qr-close-button">
-        </div>
-        <div id="qrCodeContainer"></div>
-    </div>
-
-</body></html>
 @endsection
