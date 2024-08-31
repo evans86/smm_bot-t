3f9a04af748af9ebe7ced5d9323c470ef7632c35
evans <evandaniil85@gmail.com>
2024-08-31 12:58:14 +0300

    update api

--- resources/views/activate/show/index.blade.php
+++ resources/views/activate/show/index.blade.php
@@ -1,9 +1,17 @@
 @extends('layouts.app', ['page' => __('Show'), 'pageSlug' => 'show'])
 
 @section('content')
-    <pre style="width: 1423ch; contain-intrinsic-height: 482.4em; content-visibility: visible">#profile-title: base64:VFZDIHwgTUlY
-vless://9a1a6700-5172-4555-8129-30cf7bde37d6@89.110.125.202:8443?security=reality&type=tcp&headerType=&flow=xtls-rprx-vision&path=&host=&sni=discordapp.com&fp=chrome&pbk=SbVKOEMjK0sIlbwg4akyBg5mL5KZwwB-ed4eEE7YnRc&sid=#%F0%9F%9A%80%20Marz%20%28TestUser%29%20%5BVLESS%20-%20tcp%5D
-False
+    <pre style="width: 1423ch; contain-intrinsic-height: 482.4em; content-visibility: visible">
+
+        #profile-title: base64:VFZDIHwgTUlY
+        #profile-update-interval: 1
+        #subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=2546249531
+        #support-url: https://t.me/v2raycollector
+        #profile-web-page-url: https://github.com/yebekhe/TelegramV2rayCollector
+
+        vless://9a1a6700-5172-4555-8129-30cf7bde37d6@89.110.125.202:8443?security=reality&type=tcp&headerType=&flow=xtls-rprx-vision&path=&host=&sni=discordapp.com&fp=chrome&pbk=SbVKOEMjK0sIlbwg4akyBg5mL5KZwwB-ed4eEE7YnRc&sid=#%F0%9F%9A%80%20Marz%20%28TestUser%29%20%5BVLESS%20-%20tcp%5D
+        False
+
     </pre>
 
 @endsection
