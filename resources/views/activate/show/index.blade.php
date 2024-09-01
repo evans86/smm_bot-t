9d4d5dd853b9b56c9942a2a03dd7cb35de15759a
evans <evandaniil85@gmail.com>
2024-09-01 20:58:28 +0300

    update api

--- resources/views/activate/show/index.blade.php
+++ resources/views/activate/show/index.blade.php
@@ -10,7 +10,6 @@
         #profile-web-page-url: https://github.com/yebekhe/TelegramV2rayCollector
 
         vless://9a1a6700-5172-4555-8129-30cf7bde37d6@89.110.125.202:8443?security=reality&type=tcp&headerType=&flow=xtls-rprx-vision&path=&host=&sni=discordapp.com&fp=chrome&pbk=SbVKOEMjK0sIlbwg4akyBg5mL5KZwwB-ed4eEE7YnRc&sid=#%F0%9F%9A%80%20Marz%20%28TestUser%29%20%5BVLESS%20-%20tcp%5D
-        False
 
     </pre>
 
