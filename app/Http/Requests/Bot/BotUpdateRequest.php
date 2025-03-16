8c22373ad8a60871ccc4fb55676870f8368ee732
evans <evandaniil85@gmail.com>
2025-03-16 17:10:51 +0300

    fix version

--- app/Http/Requests/Bot/BotUpdateRequest.php
+++ app/Http/Requests/Bot/BotUpdateRequest.php
@@ -21,7 +21,7 @@ class BotUpdateRequest extends FormRequest
             'id' => 'required|integer',
             'public_key' => 'required|string',
             'private_key' => 'required|string',
-            'version' => 'required|string|min:1|max:1',
+            'version' => 'required|string|min:1|max:2',
             'category_id' => 'required|integer|min:1',
             'percent' => 'required|integer|min:0',
             'color' => 'required|integer|min:1|max:6',
