e190b90f76bddf06f407f880f4f3114d40a32d27
evans86 <evansdaniil@mail.ru>
2026-04-13 20:59:43 +0300

    add notice

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('http-request-logs:prune')->daily();
+        //
     }
 
     /**
